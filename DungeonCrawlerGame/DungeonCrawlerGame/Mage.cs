880247b3ce2b6527a2b3b5c2e1e006e22296278e
KristinaMustapic <kristina.mustapic10@gmail.com>
2020-12-13 20:21:29 +0100

    Added Ranger and Mage classes

---
+++ DungeonCrawlerGame/DungeonCrawlerGame/Mage.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonCrawlerGame
+{
+    class Mage : Hero
+    {
+
+        private int currentMana;
+        private int maxMana;
+        private bool vratioIzMrtvih;
+
+        public int MaxMana { get => maxMana; set => maxMana = value; }
+        public int CurrentMana { get => currentMana; set => currentMana = value; }
+        public bool VratioIzMrtvih { get => vratioIzMrtvih; set => vratioIzMrtvih = value; }
+
+
+        public Mage(String name)
+        {
+            Name = name;
+            Experience = 0;
+            CurrentHealthPoints = MaxHealthPoints = 500;
+            Damage = 30;
+            CurrentMana = MaxMana = 50;
+            VratioIzMrtvih = false;
+        }
+
+        public Mage(String name, double HP, double damage, int mana)
+        {
+            Name = name;
+            CurrentHealthPoints = MaxHealthPoints = HP;
+            Experience = 0;
+            Damage = damage;
+            CurrentMana = MaxMana = mana;
+            VratioIzMrtvih = false;
+        }
+
+        public override bool HeroDefeated()
+        {
+            if (VratioIzMrtvih == false)
+            {
+                CurrentHealthPoints = MaxHealthPoints;
+                VratioIzMrtvih = true;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override double Napad(ref bool ponoviRundu)
+        {
+            Console.WriteLine("Upisi opciju napada:\n1 - Obican napad\n2 - Obnovi sve zivotne bodove");
+            int opcija = Convert.ToInt32(Console.ReadLine());
+            if (opcija == 1)
+            {
+                currentMana -= 5;
+                if (currentMana < 0)
+                {
+                    CurrentMana = MaxMana;
+                    return 0;
+                }
+                else
+                {
+                    return Damage;
+                }
+            }
+            else
+            {
+                currentMana -= 15;
+                if (currentMana < 0)
+                {
+                    CurrentMana = MaxMana;
+                }
+                else
+                {
+                    CurrentHealthPoints = MaxHealthPoints;
+                }
+                return 0;
+            }
+
+
+
+
+        }
+
+        public override void LevelUP()
+        {
+            MaxHealthPoints += 10;
+            Damage += 15;
+
+            MaxMana += 10;
+        }
+
+        public override void Status()
+        {
+            base.Status();
+            Console.WriteLine(String.Format("MP: {0} / {1}", CurrentMana, MaxMana));
+
+        }
+
+        public override void CollectExperience(int exp)
+        {
+            base.CollectExperience(exp);
+
+            CurrentMana = MaxMana;
+        }
+    }
+}
---
+++ DungeonCrawlerGame/DungeonCrawlerGame/Ranger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonCrawlerGame
+{
+    class Ranger : Hero
+    {
+        private double criticalChance;
+        private double stunChance;
+        public double CriticalChance { get => criticalChance; set => criticalChance = value; }
+        public double StunChance { get => stunChance; set => stunChance = value; }
+
+        public Ranger(String name)
+        {
+            Name = name;
+            Experience = 0;
+            CurrentHealthPoints = MaxHealthPoints = 750;
+            Damage = 20;
+            CriticalChance = StunChance = 0;
+        }
+
+        public Ranger(String name, double HP, double damage, int critical, int stun)
+        {
+            Name = name;
+            CurrentHealthPoints = MaxHealthPoints = HP;
+            Experience = 0;
+            Damage = damage;
+            CriticalChance = critical;
+            StunChance = stun;
+        }
+
+        public override double Napad(ref bool ponoviRundu)
+        {
+            Random random = new Random();
+            double brojRandom = random.NextDouble() * 100;
+            if (brojRandom >= 0 && brojRandom <= StunChance)
+            {
+                ponoviRundu = true;
+            }
+            else
+            {
+                ponoviRundu = false;
+            }
+
+            brojRandom = random.NextDouble() * 100;
+            if (brojRandom >= 0 && brojRandom <= CriticalChance)
+            {
+                return 2 * Damage;
+            }
+            else
+            {
+                return Damage;
+            }
+        }
+
+        public override void LevelUP()
+        {
+            MaxHealthPoints += 20;
+            Damage += 10;
+
+            CriticalChance += 2;
+            StunChance += 1;
+        }
+
+        public override void Status()
+        {
+            base.Status();
+            Console.WriteLine(String.Format("CritChance: {0}", CriticalChance));
+            Console.WriteLine(String.Format("StunChance: {0}", StunChance));
+
+        }
+    }
+}
